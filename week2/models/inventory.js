ac84edba41797fbde533f77b917be7649de46b67
tekelia-powe <tekelia.powe@bryanuniversity.edu>
2021-01-31 18:10:12 -0600

    Week 2 Assignment

---
+++ week2/models/inventory.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const inventorySchema = new Schema({
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    in_stock: {
+        type: Boolean,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Inventory', inventorySchema)
