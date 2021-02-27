e257e0bb7f671aa06da1b4080446ba44db6148da
tekelia-powe <tekelia.powe@bryanuniversity.edu>
2021-02-26 18:30:54 -0600

    Week 6 Assignment

---
+++ week6/assignment/client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React,{useContext} from 'react'
+import { Link } from 'react-router-dom'
+import {UserContext} from '../context/UserProvider.js'
+
+export default function Navbar(){
+  const { logout, token} = useContext(UserContext)
+
+  return (
+    <div className="navbar">
+      {token && <Link to="/profile">Profile</Link>}
+      <Link to="/public">Public</Link>
+    {token && <button onClick={logout}>Logout</button>}
+    </div>
+  )
+}
